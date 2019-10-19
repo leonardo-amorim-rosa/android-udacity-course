bbf8321de310300dadae7a6551ab83ae1d07554d
Leonardo Rosa <leoamorimrosa@gmail.com>
2019-10-19 19:25:01 -0300

    [About Me Project] - Add EditText, Done Button ...

--- AboutMe/app/src/main/java/br/com/leoamorimrosa/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/br/com/leoamorimrosa/aboutme/MainActivity.kt
@@ -1,12 +1,41 @@
 package br.com.leoamorimrosa.aboutme
 
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var nicknameEdit: EditText
+    lateinit var nicknameText: TextView
+    lateinit var doneButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nicknameEdit = findViewById(R.id.nickname_edit)
+        nicknameText = findViewById(R.id.nickname_text)
+
+        doneButton = findViewById(R.id.done_button)
+        doneButton.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
+        doneButton.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
